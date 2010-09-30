ea2642fe16dc717d33826555c4db525635a41cc5
Felix Schäfer <felix@fachschaften.org>
2010-09-30 15:55:09 +0200

    Solve updating mail notification values on sqlite, too much users from Project#notified_users. #6541
    Contributed by Felix Schäfer

--- db/migrate/20100129193813_update_mail_notification_values.rb
+++ db/migrate/20100129193813_update_mail_notification_values.rb
@@ -1,8 +1,8 @@
 # Patch the data from a boolean change.
 class UpdateMailNotificationValues < ActiveRecord::Migration
   def self.up
-    User.update_all("mail_notification = 'all'", "mail_notification = '1'")
-    User.update_all("mail_notification = 'only_my_events'", "mail_notification = '0'")
+    User.update_all("mail_notification = 'all'", "mail_notification IN ('1', 't')")
+    User.update_all("mail_notification = 'only_my_events'", "mail_notification IN ('0', 'f')")
   end
 
   def self.down
