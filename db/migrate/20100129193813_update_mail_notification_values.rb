811bc0863015c2d2ed87f71edaec95560ada9318
Felix Schäfer <felix@fachschaften.org>
2010-10-02 22:48:03 +0200

    Correctly update all mail_notification options. #6549

--- db/migrate/20100129193813_update_mail_notification_values.rb
+++ db/migrate/20100129193813_update_mail_notification_values.rb
@@ -1,8 +1,12 @@
 # Patch the data from a boolean change.
 class UpdateMailNotificationValues < ActiveRecord::Migration
   def self.up
-    User.update_all("mail_notification = 'all'", "mail_notification = '1'")
-    User.update_all("mail_notification = 'only_my_events'", "mail_notification = '0'")
+    User.record_timestamps = false
+    User.all.each do |u|
+      u.mail_notification = (u.mail_notification =~ /\A(1|t)\z/ ? 'all' : (u.notified_projects_ids.empty? ? 'only_my_events' : 'selected'))
+      u.save!
+    end
+    User.record_timestamps = true
   end
 
   def self.down
